593ff9f01441c53476500f12999356c47859e460
Ericles-Miller <ericlesmiller15@gmail.com>
2022-12-15 15:53:16 -0300

    responsive

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,6 +1,12 @@
 import styles from './Post.module.css';
 import { Comment } from './Comment';
 import { Avatar } from './Avatar';
+
+/**
+ * author { avatar_url : '' , name: '', role: ''}
+ * publishedAt: Date
+ * content: string
+ */
 export function Post() {
     return (
         <article className={styles.post}>
@@ -15,14 +21,7 @@ export function Post() {
                 <time title='11 de Maio de 2022' dateTime='2022-05-11 08:13:30'>Publicado há 1h</time>
             </header>
             <div className={styles.content}>
-                <p> Fala galeraa 👋</p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p>👉<a href=''> jane.design/doctorcare </a></p>
-                <p>
-                    <a href=''>#novoprojeto</a>{' '}
-                    <a href=''> #nlw</a>
-                    <a href=''> #rocketseat</a>{' '}
-                </p>
+                
             </div>
             <form className={styles.commentForm}>
                 <strong>Deixe seu Feedback</strong>
